8aedd9e9a54ddd432641d9a9a1e9470e81c63650
yuskara <you@example.com>
2020-08-22 14:33:20 -0400

    MediaPlayer one file

---
+++ MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    Context context=this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer=MediaPlayer.create(context,R.raw.cello);
+
+    }
+    public void handlePlay(View view){
+        mediaPlayer.start();
+    }
+
+    public void handlePause(View view) {
+        mediaPlayer.pause();
+    }
+    public void handleStop(View view) {
+        mediaPlayer.stop();
+    }
+}
